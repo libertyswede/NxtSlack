9067780e77d1f4974b5d5cbe6ace28f65d07e4ae
Liberty Swede <libertyswede@outlook.com>
2016-08-05 00:03:30 +0200

    Basic version of the program, might even work...

--- Program.cs
+++ Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using NxtLib;
+using NxtLib.Blocks;
+using NxtLib.ServerInfo;
+using System.Threading.Tasks;
+using System.Threading;
 
-namespace ConsoleApplication
+namespace NxtSlack
 {
     public class Program
     {
+        private const string ServerAddress = "http://node1.ardorcrypto.com:7876/nxt";
+        private static readonly List<ulong> messageTransactions = new List<ulong>();
+        private static ServerInfoService serverInfoService;
+        private static BlockService blockService;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            serverInfoService = new ServerInfoService(ServerAddress);
+            blockService = new BlockService(ServerAddress);
+            var blockChainStatus = serverInfoService.GetBlockchainStatus().Result;
+            var lastHeight = blockChainStatus.NumberOfBlocks - 1;
+
+            while (true)
+            {
+                lastHeight = ScanBlockchain(blockChainStatus.NumberOfBlocks - 1, lastHeight);
+                Thread.Sleep(10000);
+
+                blockChainStatus = serverInfoService.GetBlockchainStatus().Result;
+            }
+        }
+
+        private static int ScanBlockchain(int currentHeight, int lastHeight)
+        {
+            while (currentHeight > lastHeight)
+            {
+                var blockReply = blockService.GetBlockIncludeTransactions(BlockLocator.ByHeight(++lastHeight)).Result;
+                Console.WriteLine($"New block detected @ height: {blockReply.Height}");
+
+                foreach (var transaction in blockReply.Transactions.Where(t => t.SubType == TransactionSubType.MessagingArbitraryMessage && 
+                                                                               t.Message != null && t.Message.IsText))
+                {
+                    Console.WriteLine($"Message from {transaction.SenderRs} to {transaction.RecipientRs}");
+                    Console.WriteLine($"{transaction.Message.MessageText}");
+                }
+            }
+            return lastHeight;
         }
     }
 }
